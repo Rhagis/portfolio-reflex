7d4970fd755cfbf48c04ae7f3da1bf033d679ec7
Rhagis <37494275+Rhagis@users.noreply.github.com>
2024-02-24 10:57:36 -0300

    a

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -1,26 +1,14 @@
 /** @jsxImportSource @emotion/react */
 
 
-import { Fragment, useCallback, useContext } from "react"
+import { Fragment, useContext } from "react"
 import { EventLoopContext } from "/utils/context"
 import { Event, getBackendURL, isTrue } from "/utils/state"
-import { Button as RadixThemesButton, Code as RadixThemesCode, Dialog as RadixThemesDialog, Flex as RadixThemesFlex, Heading as RadixThemesHeading, Text as RadixThemesText, ThemePanel as RadixThemesThemePanel } from "@radix-ui/themes"
+import { Code as RadixThemesCode, Dialog as RadixThemesDialog, Flex as RadixThemesFlex, Heading as RadixThemesHeading, Text as RadixThemesText, ThemePanel as RadixThemesThemePanel } from "@radix-ui/themes"
 import env from "/env.json"
 import NextHead from "next/head"
 
 
-
-export function Button_d474754a9d7f8322123330cf11ed7fd1 () {
-  const [addEvents, connectError] = useContext(EventLoopContext);
-
-  const on_click_123926a228ab954b33ec736733dffdba = useCallback((_e) => addEvents([Event("_redirect", {path:`https://reflex.dev/docs/getting-started/introduction`,external:false})], (_e), {}), [addEvents, Event])
-
-  return (
-    <RadixThemesButton onClick={on_click_123926a228ab954b33ec736733dffdba} size={`4`}>
-  {`Check out our docs!`}
-</RadixThemesButton>
-  )
-}
 
 export function Fragment_1762bb90abdb81b879b2a22edbbe01a1 () {
   const [addEvents, connectError] = useContext(EventLoopContext);
@@ -71,7 +59,6 @@ export default function Component() {
   {`portfolio/portfolio.py`}
 </RadixThemesCode>
 </RadixThemesText>
-  <Button_d474754a9d7f8322123330cf11ed7fd1/>
 </RadixThemesFlex>
 </RadixThemesFlex>
   <NextHead>
