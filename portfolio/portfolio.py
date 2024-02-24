2dc61fbc7e338a9868300277cedb7caa1ffff6b7
Rhagis <37494275+Rhagis@users.noreply.github.com>
2024-02-24 16:12:56 -0300

    a

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -16,7 +16,7 @@ def index() -> rx.Component:
     return rx.center(
         rx.theme_panel(),
         rx.vstack(
-            rx.text("Hola esto es una prueba de reflex que no funciona aun "),
+            rx.text("Hola esto es una prueba de reflex que no funciona aun"),
             rx.heading("Welcome to Reflex!a", size="9"),
             rx.text("Get started by editing ", rx.code(filename)),
             align="center",
