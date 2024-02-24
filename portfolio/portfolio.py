c2cbef9089ff9dadb52690338375d15073ab5b36
Rhagis <37494275+Rhagis@users.noreply.github.com>
2024-02-24 10:25:55 -0300

    primer cambio

--- portfolio/portfolio.py
+++ portfolio/portfolio.py
@@ -16,7 +16,7 @@ def index() -> rx.Component:
     return rx.center(
         rx.theme_panel(),
         rx.vstack(
-            rx.text("Hola esto es una prueba"),
+            rx.text("Hola esto es una prueba de reflex"),
             rx.heading("Welcome to Reflex!a", size="9"),
             rx.text("Get started by editing ", rx.code(filename)),
             rx.button(
